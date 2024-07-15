ffdd2e10b2d501198278c423b24e840f2416b8e8
Simon <simzeru@gmail.com>
2024-07-15 15:30:44 +0200

    finalize the app

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -1,56 +1,53 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import BankCard from './BankCard'
+"use client"
 
-function RightSidebar({ user, transactions, banks }: RightSidebarProps) {
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import BankCard from './BankCard'
+import { countTransactionCategories } from '@/lib/utils'
+import Category from './Category'
+import { Button } from './ui/button'
+import PlaidLink from './PlaidLink'
+
+const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
+  const categories: CategoryCount[] = countTransactionCategories(transactions);
+  console.log("catégories : ", categories)
+
   return (
-    <aside className='right-sidebar'>
-      <section className='flex flex-col pb-8'>
-        <div className='profile-banner' />
+    <aside className="right-sidebar">
+      <section className="flex flex-col pb-8">
+        <div className="profile-banner" />
         <div className="profile">
           <div className="profile-img">
-            <span className='text-5xl font-bold text-blue-500'>
-              {user.name[0]}
-            </span>
+            <span className="text-5xl font-bold text-blue-500">{user.firstName[0]}</span>
           </div>
 
           <div className="profile-details">
             <h1 className='profile-name'>
-              {user.name}
+              {user.firstName} {user.lastName}
             </h1>
             <p className="profile-email">
               {user.email}
             </p>
           </div>
         </div>
-
       </section>
-      <section className='banks'>
-        <div className="flex w-full justify-between">
-          <h2 className="header-2">
-            My Banks
+
+      <section className="banks">
+        <div className="flex w-full justify-between items-center">
+          <h2 className="header-2">My Banks</h2>
+          <h2 className="text-14 font-semibold text-gray-600">
+            <PlaidLink user={user} variant='primary' />
           </h2>
-          <Link href="/" className='flex gap-2'>
-            <Image
-              src="/icons/plus.svg"
-              alt="plus"
-              width={20}
-              height={20}
-            />
-            <h2 className="text-14 font-semibold text-gray-600">
-              Add Bank
-            </h2>
-          </Link>
         </div>
 
-        {banks?.length > 0 &&  (
-          
+        {banks?.length > 0 && (
           <div className="relative flex flex-1 flex-col items-center justify-center gap-5">
-            <div className="relative z-10">
+            <div className='relative z-10'>
               <BankCard 
                 key={banks[0].$id}
-                account={banks[0]} 
-                userName={user.name}
+                account={banks[0]}
+                userName={`${user.firstName} ${user.lastName}`}
                 showBalance={false}
               />
             </div>
@@ -58,16 +55,25 @@ function RightSidebar({ user, transactions, banks }: RightSidebarProps) {
               <div className="absolute right-0 top-8 z-0 w-[90%]">
                 <BankCard 
                   key={banks[1].$id}
-                  account={banks[1]} 
-                  userName={user.name}
+                  account={banks[1]}
+                  userName={`${user.firstName} ${user.lastName}`}
                   showBalance={false}
                 />
               </div>
             )}
           </div>
         )}
-      </section>
 
+        <div className="mt-10 flex flex-1 flex-col gap-6">
+          <h2 className="header-2">Top categories</h2>
+
+          <div className='space-y-5'>
+            {categories.map((category, index) => (
+              <Category key={category.name} category={category} />
+            ))}
+          </div>
+        </div>
+      </section>
     </aside>
   )
 }
